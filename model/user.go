e7ef8fbdb0d4e54584f8b0533d6cb623720e5a86
TT-RR <kaiza1mage@gmail.com>
2023-05-22 14:31:21 +0900

    ユーザ情報構造体定義

--- model/user.go
+++ model/user.go
@@ -3,9 +3,14 @@ package model
 import "time"
 
 type User struct {
-	Id       uint      `json:"id"`
-	Email    string    `json:"email"`
+	Id       uint      `json:"id" gorm:"primary_key"`
+	Email    string    `json:"email" gorm:"unique"`
 	Password string    `json:"password"`
 	CreateAt time.Time `json:"create_at"`
 	UpdateAt time.Time `json:"update_at"`
+}
+
+type UserResponse struct {
+	Id    uint   `json:"id" gorm:"primary_key"`
+	Email string `json:"email" gorm:"unique"`
 }
