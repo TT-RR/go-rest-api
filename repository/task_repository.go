1a9bdacad8a747a50969fba1a65d60691d2e5f6a
TT-RR <kaiza1mage@gmail.com>
2023-06-13 10:12:54 +0900

    CreateTask実装

--- repository/task_repository.go
+++ repository/task_repository.go
@@ -4,6 +4,7 @@ import (
 	"go-rest-api/model"
 
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type ITaskRepository interface {
@@ -36,4 +37,15 @@ func (tr *taskRepository) GetTaskByID(task *model.Task, userId uint, taskId uint
 		return err
 	}
 	return nil
+}
+
+func (tr *taskRepository) CreateTask(task *model.Task) error{
+	if err := tr.db.Create(task).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (tr *taskRepository) UpdateTask(task *model.Task, userId uint, taskId uint) error{
+	result := tr.db.Model(task).Clauses(clause.Returning{}).Where("id=? AND user_id=?", taskId).Updates(map[string]interface{}{
 }
