33cb8940d9a99ad71f1b94b07cf8e0867facb8fe
TT-RR <kaiza1mage@gmail.com>
2023-06-13 11:23:27 +0900

    GetAllTasks実装

--- controller/task_controller.go
+++ controller/task_controller.go
@@ -2,7 +2,9 @@ package controller
 
 import (
 	"go-rest-api/usecase"
+	"net/http"
 
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
@@ -23,7 +25,15 @@ func NewTaskController(tu usecase.ITaskUsecase) ITaskController {
 }
 
 func (tc *taskController) GetAllTasks(c echo.Context) error {
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userId := claims["userId"]
 
+	tasksRes, err := tc.tu.GetAllTasks(uint(userId.(float64)))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, tasksRes)
 }
 
 func (tc *taskController) GetTaskById(c echo.Context) error {
