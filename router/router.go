756e3d754741de436cc87707017be65dd69c13c2
TT-RR <kaiza1mage@gmail.com>
2023-06-20 10:32:36 +0900

    "github.com/labstack/echo-jwt/v4"のインストール

--- router/router.go
+++ router/router.go
@@ -2,11 +2,13 @@ package router
 
 import (
 	"go-rest-api/controller"
+	"os"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
-func NewRouter(uc controller.IUserController) *echo.Echo {
+func NewRouter(uc controller.IUserController, tc controller.ITaskController) *echo.Echo {
 	e := echo.New()
 	//エンドポイントの追加
 	e.POST("/signup", uc.SignUp)
@@ -15,5 +17,12 @@ func NewRouter(uc controller.IUserController) *echo.Echo {
 	//Loginのエンドポイントにリクエストが来たら、Loginメソッドを実行する
 	e.POST("/logout", uc.Logout)
 	//Logoutのエンドポイントにリクエストが来たら、Logoutメソッドを実行する
+
+	//tasksのエンドポイントにリクエストが来たら、echojwt.WithConfigを実行する
+	t := e.Group("/tasks")
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte(os.Getenv("SECRET")),
+		TokenLookup: "cookie:token",
+	}))
 	return e
 }
