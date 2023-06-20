5cc877492e04b075907e70de1bce79fc161593ec
TT-RR <kaiza1mage@gmail.com>
2023-06-20 10:17:20 +0900

    CreateTask実装

--- controller/task_controller.go
+++ controller/task_controller.go
@@ -1,6 +1,7 @@
 package controller
 
 import (
+	"go-rest-api/model"
 	"go-rest-api/usecase"
 	"net/http"
 	"strconv"
@@ -51,12 +52,31 @@ func (tc *taskController) GetTaskById(c echo.Context) error {
 	return c.JSON(http.StatusOK, tasksRes)
 }
 
+// taskを作成する
 func (tc *taskController) CreateTask(c echo.Context) error {
-	
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userId := claims["user_id"]
+
+	task := model.Task{}
+	//リクエストボディを取得構造体に変換(Bind)
+	if err := c.Bind(&task); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	task.UserID = uint(userId.(float64))
+	tasksRes, err := tc.tu.CreateTask(task)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusCreated, tasksRes)
 }
 
 func (tc *taskController) UpdateTask(c echo.Context) error {
-
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	userId := claims["user_id"]
+	id := c.Param("taskId")
+	taskId, _ := strconv.Atoi(id)
 }
 
 func (tc *taskController) DeleteTask(c echo.Context) error {
