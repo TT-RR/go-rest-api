6642334dd81ba40cbf1196ef12895a1ba298c645
TT-RR <kaiza1mage@gmail.com>
2023-07-07 12:58:02 +0900

    TaskValidator実装

--- validator/task_validator.go
+++ validator/task_validator.go
@@ -1,6 +1,10 @@
 package validator
 
-import "go-rest-api/model"
+import (
+	"go-rest-api/model"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
 
 type ITaskValidator interface {
 	TaskValidator(task model.Task) error
@@ -12,4 +16,12 @@ func NewTaskValidator() ITaskValidator {
 	return &taskValidator{}
 }
 
-func (tv *taskValidator) TaskValidator(task model.Task) error
+func (tv *taskValidator) TaskValidator(task model.Task) error {
+	return validation.ValidateStruct(&task,
+		validation.Field(
+			&task.Title,
+			validation.Required.Error("タイトルは必須です"),
+			validation.RuneLength(1, 10).Error("タイトルは1文字以上10文字以下です"),
+		),
+	)
+}
