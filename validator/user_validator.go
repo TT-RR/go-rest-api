496eea1e42b0525e8243d47a43ff237335f7f964
TT-RR <kaiza1mage@gmail.com>
2023-07-07 12:58:29 +0900

    init

--- validator/user_validator.go
+++ validator/user_validator.go
@@ -1,1 +1,34 @@
 package validator
+
+import (
+	"go-rest-api/model"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+	"github.com/go-ozzo/ozzo-validation/v4/is"
+)
+
+type IUserValidator interface {
+	UserValidator(user model.User) error
+}
+
+type userValidator struct{}
+
+func NewUserValidator() IUserValidator {
+	return &userValidator{}
+}
+
+func (uv *userValidator) UserValidator(user model.User) error {
+	return validation.ValidateStruct(&user,
+		validation.Field(
+			&user.Email,
+			validation.Required.Error("メールアドレスは必須です"),
+			validation.RuneLength(1, 30).Error("メールアドレスは1文字以上30文字以下です"),
+			is.Email.Error("メールアドレスの形式が不正です"),
+		),
+		validation.Field(
+			&user.Password,
+			validation.Required.Error("パスワードは必須です"),
+			validation.RuneLength(6, 30).Error("パスワードは6文字以上30文字以下です"),
+		),
+	)
+}
